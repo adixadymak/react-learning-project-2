fcbee87b8763dee74042c5681610f8d9fb1d56d9
adixadymak <adixadymak@gmail.com>
2024-07-27 12:07:43 +0100

    User Input & Two-Way-Binding
    Getting data from the input form and feeding it back to the same input form itself

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
@@ -11,16 +12,23 @@ export default function Player({ name, symbol }) {
     // setIsEditing(isEditing ? false : true);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChange(event) {
+    console.log(event.target.value);
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
