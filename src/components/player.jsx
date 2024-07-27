b8653bcc10d3099f7e33b964a6d5c4e6aa5478be
adixadymak <adixadymak@gmail.com>
2024-07-27 04:02:44 +0100

    Exercise: Working with State
    I added the save and edit button which is not part of the exercise

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+  function handleSaveClick() {
+    setIsEditing(false);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing ? (
+          <span className="player-name">{name}</span>
+        ) : (
+          <input type="text" />
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      {!isEditing ? (
+        <button onClick={handleEditClick}>Edit</button>
+      ) : (
+        <button onClick={handleSaveClick}>Save</button>
+      )}
     </li>
   );
 }
