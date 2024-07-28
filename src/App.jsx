c33774fd641f7a3b3afc1520ac69cd9fbdebed1a
adixadymak <adixadymak@gmail.com>
2024-07-28 08:01:12 +0100

    Deriving Computed Values From Other Computed Values

--- src/App.jsx
+++ src/App.jsx
@@ -35,8 +35,23 @@ function App() {
     gameBoard[row][col] = player;
   }
 
+  let winner;
+
   for (const combinations of WINNING_COMBINATIONS) {
-    //  const firstSquareSymbol
+    const firstSquareSymbol =
+      gameBoard[combinations[0].row][combinations[0].column];
+    const secondSquareSymbol =
+      gameBoard[combinations[1].row][combinations[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combinations[2].row][combinations[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
   }
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -70,6 +85,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
+        {winner && <p>You won, {winner}! </p>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
